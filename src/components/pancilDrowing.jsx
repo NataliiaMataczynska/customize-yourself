cf79182f88e4da4ea2f2d4ffc4c2d85a11f03ae2
NataliiaMataczynska <sozanskanatalya@gmail.com>
2023-12-04 20:01:11 +0100

    nothing to commit

--- src/components/pancilDrowing.jsx
+++ src/components/pancilDrowing.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {useState }from 'react';
+import { img1, img2, img3 } from 'src/assets/images/pancil_drowing/'
 
+document.getElementById('pancil-1').src = img1
+document.getElementById('pancil-2').src = img2
+document.getElementById('pancil-3').src = img3
 export default function PancilDrowing() {
     const imagesPancilDrowing = [
         'src/assets/images/pancil_drowing/pancil-1.jpg',
@@ -14,6 +18,13 @@ export default function PancilDrowing() {
         'src/assets/images/pancil_drowing/pancil-8.jpg',
     ];
 
+    const [pancilImages, setPancilImages] = useState([]);
+
+    useEffect(() => {
+        setPancilImages(imagesPancilDrowing);
+    }, []);
+
+
     return (
         <div className="juckets-container" >
             <div className="jucketse-text">
@@ -27,7 +38,7 @@ export default function PancilDrowing() {
                 <div className="juckets-images grid-images">
                     <ul className="images-group">
                         {
-                            imagesPancilDrowing.map((e, index) => (
+                            pancilImages.map((e, index) => (
                                 <li key={index} className="image-group">
                                     <img src={e} className="image-realizations" />
                                 </li>
